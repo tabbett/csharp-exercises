f9a1f6e87dd875af52aabd1d3acd1b916d5b4226
Tammy Abbett <tammylabbett@gmail.com>
2017-02-27 23:25:37 -0600

    Hello World and area

---
+++ area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius:");
+            string radius = Console.ReadLine();
+            int radiusNum = int.Parse(radius);
+            double area = Math.PI * radiusNum * radiusNum;
+            string areaStr = area.ToString();
+            Console.WriteLine("The area of a circle with radius "+ radius + " is: " + areaStr);
+            Console.ReadLine();
+        }
+    }
+}
